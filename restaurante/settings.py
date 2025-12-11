26062cfa0b58e00e5a611cc4baa8537eaa38f325
Diego Torres <159590169+akadegodev@users.noreply.github.com>
2025-12-11 00:04:00 -0600

    Update ALLOWED_HOSTS for production deployment

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-+&hr8kuox!lbw)!p&!+ok
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', '*', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', '*', 'backend-wvn4.onrender.com', default='localhost,127.0.0.1', cast=Csv())
 
 
 # Application definition
